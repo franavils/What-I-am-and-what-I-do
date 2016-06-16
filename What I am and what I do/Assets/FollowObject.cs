2f2b2096c26f0560d57f440324ec141dc380260f
franavils <aviles110@hotmail.com>
2016-06-16 13:55:07 +0200

    Dashing connected to light

---
+++ What I am and what I do/Assets/FollowObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour {
+
+    public GameObject ObjectToFollow;
+    public float YOffset;
+    public float XOffset;
+    public float ZOffset;
+
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = new Vector3(XOffset, YOffset, ZOffset);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = (ObjectToFollow.transform.position + offset);
+	
+	}
+}
--- What I am and what I do/Assets/Scripts/PlayerController.cs
+++ What I am and what I do/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,11 @@ public class PlayerController : MonoBehaviour {
     public bool Circle;
     public bool Triangle;
 
+    public float SquareGravity;
+    public float CircleGravity;
+    public float TriangleGravity;
+
+
     public GameObject SquareShape;
     public GameObject CircleShape;
     public GameObject TriangleShape;
@@ -20,9 +25,31 @@ public class PlayerController : MonoBehaviour {
     public CircleCollider2D CircleCollider;
     public PolygonCollider2D TriangleCollider;
 
+    //Lights
+    public GameObject Light2D;
+    public Color Blue = new Color32(84, 170, 255, 43);
+    public Color Red = new Color32(255, 84, 84, 43);
+    public Color Green = new Color32(84, 255, 170, 43);
+    public float MaxLightRadius;
+
     GameObject activeShape;
     //Movement
-    public float HorizontalSpeed;
+    //Square
+    public float HorizontalSpeedSquare;
+    public float MaxHorizontalSpeedSquare;
+    public bool Dashing;
+    public bool DashingReady;
+    public float DashReloadTime;
+    public float CurrentDashReloadTime;
+    public float DashingDuration;
+    public float CurrentTimeDashingDuration;
+    public float DashPush;
+    public TrailRenderer DashTrail;
+
+    //Circle
+    public float HorizontalSpeedCircle;
+
+
     float currentYVelocity;
     // Use this for initialization
     void Start () {
@@ -30,6 +57,11 @@ public class PlayerController : MonoBehaviour {
         anim = GetComponent<Animator>();
         Square = true;
         anim.SetBool("Square", true);
+        DashingReady = true;
+        DashTrail = gameObject.GetComponent<TrailRenderer>();
+        DashTrail.enabled = false;
+
+
 
         SquareCollider = gameObject.GetComponent<BoxCollider2D>();
         CircleCollider = gameObject.GetComponent<CircleCollider2D>();
@@ -43,12 +75,12 @@ public class PlayerController : MonoBehaviour {
         //CircleShape.SetActive(false);
         //TriangleShape.SetActive(false);
 
-        activeShape = SquareShape;
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-        currentYVelocity = rb.velocity.y;
+        //currentYVelocity = rb.velocity.y;
         
 
         if (Input.GetButtonDown("Square"))
@@ -90,7 +122,7 @@ public class PlayerController : MonoBehaviour {
             CircleCollider.enabled = true;
             TriangleCollider.enabled = false;
 
-            activeShape = CircleShape;
+            
         }
         if (Input.GetButtonDown("Triangle"))
         {
@@ -109,28 +141,151 @@ public class PlayerController : MonoBehaviour {
             CircleCollider.enabled = false;
             TriangleCollider.enabled = true;
 
-            activeShape = TriangleShape;
+            
         }
-        gameObject.transform.position = activeShape.transform.position;
+
 
         if (Square)
         {
-            rb.gravityScale = 2.0f;
-            float horizontalMovement = Input.GetAxis("Horizontal");
-
             
-            rb.velocity = new Vector2(horizontalMovement * HorizontalSpeed, currentYVelocity);
+            
+            if (Light2D.GetComponent<DynamicLight>().LightRadius >= MaxLightRadius)
+            {
+                DashingReady = true;
+            } else if (Light2D.GetComponent<DynamicLight>().LightRadius < MaxLightRadius)
+            {
+                DashingReady = false;
+            }
+
+            Light2D.GetComponent<DynamicLight>().LightMaterial.SetColor("_Color", Blue);
+
+            //Dashing
+            if (DashingReady && Input.GetButtonDown("X"))
+            {
+                Dashing = true;
+                
+            }
+            if (Dashing == false)
+            {
+                rb.gravityScale = SquareGravity;
+                if (Light2D.GetComponent<DynamicLight>().LightRadius < MaxLightRadius)
+                {
+                    Light2D.GetComponent<DynamicLight>().LightRadius += Time.deltaTime *4;
+                }
+            }
+            
+            if (Dashing)
+            {
+
+                rb.gravityScale = 0;
+                DashTrail.enabled = true;
+                Light2D.GetComponent<DynamicLight>().LightRadius -= 0.6f;
+                CurrentTimeDashingDuration += Time.deltaTime;
+            if (CurrentTimeDashingDuration >= DashingDuration)
+            {
+                    
+                    Dashing = false;
+                    DashTrail.enabled = false;
+                    CurrentTimeDashingDuration = 0;
+                    
+            }
+            }
+           
+
+            //float horizontalMovement = Input.GetAxis("Horizontal");
+
+
+            //rb.velocity = new Vector2(horizontalMovement * HorizontalSpeed, currentYVelocity);
+
+        }
+        if (Triangle)
+        {
+            rb.gravityScale = TriangleGravity;
+            Light2D.GetComponent<DynamicLight>().LightMaterial.SetColor("_Color", Green);
+            if (Light2D.GetComponent<DynamicLight>().LightRadius < MaxLightRadius)
+            {
+                Light2D.GetComponent<DynamicLight>().LightRadius += Time.deltaTime * 4;
+            }
 
         }
         if (Circle)
         {
-            rb.gravityScale = -0.5f;
+            rb.gravityScale = CircleGravity;
+            Light2D.GetComponent<DynamicLight>().LightMaterial.SetColor("_Color", Red);
+            if (Light2D.GetComponent<DynamicLight>().LightRadius < MaxLightRadius)
+            {
+                Light2D.GetComponent<DynamicLight>().LightRadius += Time.deltaTime * 4;
+            }
+
+        }
+        
+
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        Vector2 movingHorizontal = new Vector2(horizontalMovement, 0);
+        float currentYSpeed = rb.velocity.y;
+        if (Square)
+        {
+            rb.isKinematic = false;
             
+            if (Dashing)
+            {
+                Vector2 currentVelocity = new Vector2(rb.velocity.x, 0);
+                rb.AddForce(currentVelocity.normalized * DashPush, ForceMode2D.Impulse);
+            } 
+            if (Dashing == false)
+            {
+                if (Mathf.Abs(rb.velocity.x) > MaxHorizontalSpeedSquare)
+                {
+                    if (rb.velocity.x > 0)
+                    {
+                        rb.velocity = new Vector2(MaxHorizontalSpeedSquare, currentYSpeed);
+                    }
+                    if (rb.velocity.x < 0)
+                    {
+                        rb.velocity = new Vector2(-MaxHorizontalSpeedSquare, currentYSpeed);
+                    }
+
+                }
+            }
+            
+
+            
+
+            if (Mathf.Abs(rb.velocity.y) > 0)
+            {
+                
+            } else if (Mathf.Abs(rb.velocity.y) == 0)
+            {
+                rb.AddForce(movingHorizontal * HorizontalSpeedSquare);
+            }
+
+
+
+            
+
+        }
+        if (Circle)
+        {
+            rb.AddForce(movingHorizontal * HorizontalSpeedCircle);
+            rb.isKinematic = false;
+
         }
         if (Triangle)
         {
+            //Vector2 noForce = new Vector2(0, 0);
+            //rb.velocity = noForce;
+            //rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+            //rb.constraints = RigidbodyConstraints2D.FreezePositionX;
             
+            rb.isKinematic = true;
+            //rb.rotation += 10;
             
+
         }
+
     }
 }
